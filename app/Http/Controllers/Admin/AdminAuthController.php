b5b20086035bce9193254eb8444c9bd81952baed
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-06 10:04:51 +0200

    Add login functionality and redirect middleware for admin authentication

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -7,9 +7,19 @@ use Illuminate\Http\Request;
 
 class AdminAuthController extends Controller {
 	public function showLoginForm() {
+		return inertia( 'Admin/Auth/login' );
 	}
-	public function login() {
+	public function login( Request $request ) {
+		if ( auth()->attempt(
+			[ 'email' => $request->email, 'password' => $request->password, 'isAdmin' => 1 ]
+		) ) {
+			return redirect()->route( 'admin.dashboard' );
+		}
+		return redirect()->route( 'admin.login' )->with( 'error', 'Invalid Credentials' );
 	}
 	public function logout() {
+		auth()->logout();
+		session()->invalidate();
+		return redirect()->route( 'admin.login' );
 	}
 }
---
+++ app/Http/Middleware/RedirectAdmin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectAdmin {
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+	 */
+	public function handle( Request $request, Closure $next, $quard = null ): Response {
+		if ( auth()->guard( $quard )->check() && auth()->user()->isAdmin ) {
+			return redirect()->route( 'admin.dashboard' );
+		}
+		return $next( $request );
+	}
+}
